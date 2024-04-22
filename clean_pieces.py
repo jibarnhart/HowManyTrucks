9eb51c0a53e91386af79383c2a74f6ccacdb934c
Jarod Barnhart <jarod.barnhart@gmail.com>
2024-04-22 13:37:49 -0400

    add cleaning function

---
+++ clean_pieces.py
@@ -0,0 +1,29 @@
+def cleaning(pieces, needed_equipment):
+
+    for piece in pieces:
+        if needed_equipment in ['dry van', 'reefer']:
+            if piece['stackable']:
+                piece['modified_height'] = piece['height']
+            else:
+                piece['modified_height'] = 108
+            piece['modified_width'] = piece['width']
+            
+        elif needed_equipment == 'flatbed':
+            if piece['stackable']:
+                piece['modified_height'] = piece['height']
+            else:
+                piece['modified_height'] = 102
+
+            if piece['height'] > 102:
+                piece['over_height'] = True
+            else:
+                piece['over_height'] = False
+
+            if piece['width'] > 102:
+                piece['over_width'] = True
+                piece['modified_width'] = 102
+            else:
+                piece['over_width'] = False
+                piece['modified_width'] = piece['width']
+
+    return pieces
--- how_many_trucks.py
+++ how_many_trucks.py
@@ -2,14 +2,10 @@ from py3dbp import Packer, Bin, Item
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import random
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-DRY_VAN = {
-    "length": 631,
-    "width": 100,
-    "height": 111,
-    "square_inches": 63100,
-    "cubic_inches": 7004100
-}
+from clean_pieces import cleaning
 
 PIECES = [
   {
@@ -18,7 +14,7 @@ PIECES = [
     "depth": 171,
     "width": 86,
     "height": 106,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "CLTC CONSERVATOR",
@@ -26,7 +22,7 @@ PIECES = [
     "depth": 90,
     "width": 43,
     "height": 42,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "AIR VENT PIPE LINE",
@@ -34,7 +30,7 @@ PIECES = [
     "depth": 153,
     "width": 74,
     "height": 31,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "HV BUSHING (HT)",
@@ -42,7 +38,7 @@ PIECES = [
     "depth": 193,
     "width": 30,
     "height": 46,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "HV BUSHING (CT)",
@@ -50,7 +46,7 @@ PIECES = [
     "depth": 193,
     "width": 30,
     "height": 46,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "HV BUSHING (HT)",
@@ -58,7 +54,7 @@ PIECES = [
     "depth": 193,
     "width": 30,
     "height": 46,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "MVN BUSHING",
@@ -66,7 +62,7 @@ PIECES = [
     "depth": 70,
     "width": 23,
     "height": 28,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "XV & XVN BUSHING",
@@ -74,7 +70,7 @@ PIECES = [
     "depth": 82,
     "width": 62,
     "height": 30,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "YV & YVN BUSHING",
@@ -82,7 +78,7 @@ PIECES = [
     "depth": 82,
     "width": 62,
     "height": 30,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "HV BUSHING HOUSING",
@@ -90,7 +86,7 @@ PIECES = [
     "depth": 122,
     "width": 51,
     "height": 53,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "MVN BUSHING HOUSING",
@@ -98,7 +94,7 @@ PIECES = [
     "depth": 33,
     "width": 33,
     "height": 34,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "XV & XVN BUSHING HOUSING",
@@ -106,7 +102,7 @@ PIECES = [
     "depth": 121,
     "width": 41,
     "height": 40,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "XV & XVN BUSHING HOUSING",
@@ -114,7 +110,7 @@ PIECES = [
     "depth": 121,
     "width": 41,
     "height": 40,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -122,7 +118,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -130,7 +126,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -138,7 +134,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -146,7 +142,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -154,7 +150,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -162,7 +158,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -170,7 +166,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR",
@@ -178,7 +174,7 @@ PIECES = [
     "depth": 114,
     "width": 74,
     "height": 59,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR BANK",
@@ -186,7 +182,7 @@ PIECES = [
     "depth": 169,
     "width": 129,
     "height": 53,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR BANK PIPE LINE",
@@ -194,7 +190,7 @@ PIECES = [
     "depth": 121,
     "width": 82,
     "height": 38,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR BANK OIL PUMP PIPE LINE",
@@ -202,7 +198,7 @@ PIECES = [
     "depth": 47,
     "width": 43,
     "height": 32,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR BANK SUPPORT (1)",
@@ -210,7 +206,7 @@ PIECES = [
     "depth": 220,
     "width": 94,
     "height": 43,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "RADIATOR BANK SUPPORT (2)",
@@ -218,7 +214,7 @@ PIECES = [
     "depth": 153,
     "width": 74,
     "height": 35,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "FAN SUPPORT & CABLE PIPE & ETC SUPPORT",
@@ -226,7 +222,7 @@ PIECES = [
     "depth": 121,
     "width": 62,
     "height": 52,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "COOLING FAN",
@@ -234,7 +230,7 @@ PIECES = [
     "depth": 100,
     "width": 68,
     "height": 60,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "PRESSURE RELIEF DEVICE PIPE LINE",
@@ -242,7 +238,7 @@ PIECES = [
     "depth": 149,
     "width": 62,
     "height": 33,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "XV & YV LA SUPPORT",
@@ -250,7 +246,7 @@ PIECES = [
     "depth": 137,
     "width": 66,
     "height": 32,
-    "modified_height": 106
+    "stackable": False
   },
   {
     "name": "ACCESORY",
@@ -258,28 +254,55 @@ PIECES = [
     "depth": 62,
     "width": 54,
     "height": 30,
-    "modified_height": 106
+    "stackable": False
   }
 ]
 
-def create_trailer_object(width, length, height, offset, truck_number):
-    """ This creates a list of the 8 vertices that define a trailer's cuboid. This cuboid represents the trailer being packed.
-    width: number. The width of the trailer
-    length: number. The length of the trailer
-    height: number. The height of the trailer
-    offset: How far apart each trailer cuboid should be.
-    truck_number: the number of the truck being packed
-    """
-    vertices = [
+def get_dimensions(equipment):
+
+  if equipment in ['dry van', 'reefer']:
+     
+    return [100, 111, 631, 45000]
+  
+  elif equipment in ['flatbed']:    
+
+    return [102, 102, 576, 46000]    
+
+def create_trailer_object(equipment, offset, truck_number):
+    
+    if equipment in ['dry van', 'reefer']:
+
+      width = DRY_VAN['width']
+      height = DRY_VAN['height']
+      length = DRY_VAN['length']
+
+      vertices = [
         (offset * truck_number, 0, 0),                      # Vertex 1
         (width + (offset * truck_number), 0, 0),                  # Vertex 2
-        (width + (offset * truck_number), height, 0),             # Vertex 3
-        (offset * truck_number, height, 0),                 # Vertex 4
-        (offset * truck_number, 0, length),                 # Vertex 5
-        (width + (offset * truck_number), 0, length),             # Vertex 6
-        (width + (offset * truck_number), height, length),        # Vertex 7
-        (offset * truck_number, height, length)             # Vertex 8
-    ]
+        (width + (offset * truck_number), length, 0),             # Vertex 3
+        (offset * truck_number, length, 0),                 # Vertex 4
+        (offset * truck_number, 0, height),                 # Vertex 5
+        (width + (offset * truck_number), 0, height),             # Vertex 6
+        (width + (offset * truck_number), length, height),        # Vertex 7
+        (offset * truck_number, length, height)             # Vertex 8
+      ]
+    elif equipment in ['flatbed']:
+
+      width = 102
+      height = 102
+      length = 576
+
+      vertices = [
+        (offset * truck_number, 0, -12),                     
+        (width + (offset * truck_number), 0, -12),                  
+        (width + (offset * truck_number), length, -12),            
+        (offset * truck_number, length, -12),                
+        (offset * truck_number, 0, 0),                 
+        (width + (offset * truck_number), 0, 0),            
+        (width + (offset * truck_number), length, 0),     
+        (offset * truck_number, length, 0)            
+      ]
+
     return vertices
 
 def find_piece(name):
@@ -299,13 +322,13 @@ def create_box_object(truck_number, item, offset):
     piece = find_piece(item.name)
 
     corner_1 = [float(item.position[0])+truck_number*offset, float(item.position[2]), float(item.position[1])] #width, depth, height
-    corner_2 = [float(item.position[0])+float(item.width)+truck_number*offset, float(item.position[2]), float(item.position[1])]
+    corner_2 = [float(item.position[0])+float(piece['width'])+truck_number*offset, float(item.position[2]), float(item.position[1])]
     corner_4 = [float(item.position[0])+truck_number*offset, float(item.position[2])+float(item.depth), float(item.position[1])]
-    corner_3 = [float(item.position[0])+truck_number*offset+float(item.width), float(item.position[2])+float(item.depth), float(item.position[1])]
+    corner_3 = [float(item.position[0])+truck_number*offset+float(piece['width']), float(item.position[2])+float(item.depth), float(item.position[1])]
     corner_5 = [float(item.position[0])+truck_number*offset, float(item.position[2]), float(item.position[1])+float(piece['height'])] #width, depth, height
-    corner_6 = [float(item.position[0])+float(item.width)+truck_number*offset, float(item.position[2]), float(item.position[1])+float(piece['height'])]
+    corner_6 = [float(item.position[0])+float(piece['width'])+truck_number*offset, float(item.position[2]), float(item.position[1])+float(piece['height'])]
     corner_8 = [float(item.position[0])+truck_number*offset, float(item.position[2])+float(item.depth), float(item.position[1])+float(piece['height'])]
-    corner_7 = [float(item.position[0])+float(item.width)+truck_number*offset, float(item.position[2])+float(item.depth), float(item.position[1])+float(piece['height'])]
+    corner_7 = [float(item.position[0])+float(piece['width'])+truck_number*offset, float(item.position[2])+float(item.depth), float(item.position[1])+float(piece['height'])]
 
     vertices = np.array([corner_1, corner_2, corner_3, corner_4, corner_5, corner_6, corner_7, corner_8])
 
@@ -317,23 +340,32 @@ def plot_cuboids(vertices_list, names):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
+    colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+
     for vertices, name in zip(vertices_list, names or ['']*len(vertices_list)):
+        random_index = random.randint(0, len(colors)-1)
+        color = colors[random_index]
+
         for i in range(0, 4):
+            
+
             ax.plot([vertices[i][0], vertices[(i+1)%4][0]],
                     [vertices[i][1], vertices[(i+1)%4][1]],
-                    [vertices[i][2], vertices[(i+1)%4][2]], 'r-')
+                    [vertices[i][2], vertices[(i+1)%4][2]], color='black')
 
+            
             ax.plot([vertices[i+4][0], vertices[((i+1)%4)+4][0]],
                     [vertices[i+4][1], vertices[((i+1)%4)+4][1]],
-                    [vertices[i+4][2], vertices[((i+1)%4)+4][2]], 'r-')
+                    [vertices[i+4][2], vertices[((i+1)%4)+4][2]], color='black')
 
             ax.plot([vertices[i][0], vertices[i+4][0]],
                     [vertices[i][1], vertices[i+4][1]],
-                    [vertices[i][2], vertices[i+4][2]], 'r-')
+                    [vertices[i][2], vertices[i+4][2]], color='black')
             
             centroid = np.mean(vertices, axis=0)
             
-            ax.text(centroid[0], centroid[1], centroid[2], name, fontsize=5)
+            ax.text(centroid[0], centroid[1], centroid[2], name, fontsize=5, horizontalalignment="center")
+
             
     ax.set_xlim([0,1000])
     ax.set_ylim([0,1000])
@@ -341,15 +373,19 @@ def plot_cuboids(vertices_list, names):
     
     plt.show()
 
-def __main__():
+def __main__(equipment, pieces):
 
     packer = Packer()
 
-    for i in range(10):
-        packer.add_bin(Bin(f'53-ft-dry-van-{i+1}', 100, 111, 631, 45000))
+    pieces = cleaning(pieces, equipment)
 
-    for piece in PIECES:
-        packer.add_item(Item(piece['name'], piece['width'], piece['modified_height'], piece['depth'], piece['weight']))
+    trailer_dimensions = get_dimensions(equipment)
+
+    for i in range(10):
+        packer.add_bin(Bin(f'{equipment} {i+1}', trailer_dimensions[0], trailer_dimensions[1], trailer_dimensions[2], trailer_dimensions[3]))
+
+    for piece in pieces:
+        packer.add_item(Item(piece['name'], piece['modified_width'], piece['modified_height'], piece['depth'], piece['weight']))
 
     packer.pack(distribute_items=True)
 
@@ -358,17 +394,19 @@ def __main__():
     
     j = 0
     for b in packer.bins:
+      if len(b.items) > 0:
+        print(f"The following items will go on {b.name}")
         for item in b.items:
-            if len(b.items) > 0:
-                van = create_trailer_object(100, 111, 631, 210, j)
-                vertices_list.append(van)
-                names.append("")
-                vertices = create_box_object(j, item, 210)
-                vertices_list.append(vertices)
-                names.append(item.name)
-        j=j+1
+            print("--->", item.name)
+            trailer = create_trailer_object(equipment, 210, j)
+            vertices_list.append(trailer)
+            names.append("")
+            vertices = create_box_object(j, item, 210)
+            vertices_list.append(vertices)
+            names.append(item.name)
+      j=j+1
     plot_cuboids(vertices_list, names)
         
     return
 
-__main__()
+__main__('flatbed', PIECES)
