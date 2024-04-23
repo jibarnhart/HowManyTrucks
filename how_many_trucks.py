860c49358ac77ab547a6f7657bc2b24f09716b08
Jarod Barnhart <jarod.barnhart@gmail.com>
2024-04-23 08:42:10 -0400

    Use forked version of 3d bin packing

--- how_many_trucks.py
+++ how_many_trucks.py
@@ -1,9 +1,6 @@
-from py3dbp import Packer, Bin, Item
+from src.py3dbp.py3dbp import Packer, Bin, Item
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import random
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 from clean_pieces import cleaning
 
@@ -12,7 +9,7 @@ PIECES = [
     "name": "MAIN CONSERVATOR",
     "weight": 6460,
     "depth": 171,
-    "width": 86,
+    "width": 105,
     "height": 106,
     "stackable": False
   },
@@ -251,8 +248,8 @@ PIECES = [
   {
     "name": "ACCESORY",
     "weight": 1182,
-    "depth": 62,
-    "width": 54,
+    "depth": 60,
+    "width": 113,
     "height": 30,
     "stackable": False
   }
@@ -268,29 +265,26 @@ def get_dimensions(equipment):
 
     return [102, 102, 576, 46000]    
 
-def create_trailer_object(equipment, offset, truck_number):
+def create_trailer_object(trailer_dimensions, equipment, offset, truck_number):
+    
+    width = trailer_dimensions[0]
+    height = trailer_dimensions[1]
+    length = trailer_dimensions[2]
     
     if equipment in ['dry van', 'reefer']:
 
-      width = DRY_VAN['width']
-      height = DRY_VAN['height']
-      length = DRY_VAN['length']
-
       vertices = [
-        (offset * truck_number, 0, 0),                      # Vertex 1
+        (offset * truck_number, 0, 0),                            # Vertex 1
         (width + (offset * truck_number), 0, 0),                  # Vertex 2
         (width + (offset * truck_number), length, 0),             # Vertex 3
-        (offset * truck_number, length, 0),                 # Vertex 4
-        (offset * truck_number, 0, height),                 # Vertex 5
+        (offset * truck_number, length, 0),                       # Vertex 4
+        (offset * truck_number, 0, height),                       # Vertex 5
         (width + (offset * truck_number), 0, height),             # Vertex 6
         (width + (offset * truck_number), length, height),        # Vertex 7
-        (offset * truck_number, length, height)             # Vertex 8
+        (offset * truck_number, length, height)                   # Vertex 8
       ]
-    elif equipment in ['flatbed']:
 
-      width = 102
-      height = 102
-      length = 576
+    elif equipment in ['flatbed']:
 
       vertices = [
         (offset * truck_number, 0, -12),                     
@@ -312,23 +306,43 @@ def find_piece(name):
         if piece['name'] == name:
             return piece
 
-def create_box_object(truck_number, item, offset):
+def create_box_object(truck_number, item, width_offset, depth_offset, rotation_type):
     """ This creates a list of the 8 vertices that define an item's cuboid. These cuboids represent the item being packed into the truck.
     truck_number: int. Which truck the item goes into.
     item: the item object from the bin.
     offset: how far each truck cuboid is from each other.
+    rotation_type: integer. 0 represents the depth of the object being parallel to the sides of the trailer. 1 is rotating it 90 degrees.
     """
 
     piece = find_piece(item.name)
+    truck_offset = float(truck_number * width_offset)
+    unfitted_offset = float(depth_offset)
 
-    corner_1 = [float(item.position[0])+truck_number*offset, float(item.position[2]), float(item.position[1])] #width, depth, height
-    corner_2 = [float(item.position[0])+float(piece['width'])+truck_number*offset, float(item.position[2]), float(item.position[1])]
-    corner_4 = [float(item.position[0])+truck_number*offset, float(item.position[2])+float(item.depth), float(item.position[1])]
-    corner_3 = [float(item.position[0])+truck_number*offset+float(piece['width']), float(item.position[2])+float(item.depth), float(item.position[1])]
-    corner_5 = [float(item.position[0])+truck_number*offset, float(item.position[2]), float(item.position[1])+float(piece['height'])] #width, depth, height
-    corner_6 = [float(item.position[0])+float(piece['width'])+truck_number*offset, float(item.position[2]), float(item.position[1])+float(piece['height'])]
-    corner_8 = [float(item.position[0])+truck_number*offset, float(item.position[2])+float(item.depth), float(item.position[1])+float(piece['height'])]
-    corner_7 = [float(item.position[0])+float(piece['width'])+truck_number*offset, float(item.position[2])+float(item.depth), float(item.position[1])+float(piece['height'])]
+    #The 'pivot' point is the bottom left corner, which is the corner closes to the passenger side of the trailer, closest to the cab.
+    pivot_x_position = float(item.position[0]) + truck_offset 
+    pivot_y_position = float(item.position[2]) + unfitted_offset
+    pivot_z_position = float(item.position[1]) 
+
+    if rotation_type == 0:
+       
+      width = float(piece['width'])
+      depth = float(piece['depth'])
+      height = float(piece['height'])
+
+    elif rotation_type == 1:
+       
+      width = float(piece['depth'])
+      depth = float(piece['width'])
+      height = float(piece['height'])
+    
+    corner_1 = [pivot_x_position, pivot_y_position, pivot_z_position]
+    corner_2 = [pivot_x_position + width, pivot_y_position, pivot_z_position]
+    corner_4 = [pivot_x_position, pivot_y_position + depth, pivot_z_position]
+    corner_3 = [pivot_x_position + width, pivot_y_position + depth, pivot_z_position]
+    corner_5 = [pivot_x_position, pivot_y_position, pivot_z_position + height]
+    corner_6 = [pivot_x_position + width, pivot_y_position, pivot_z_position + height]
+    corner_8 = [pivot_x_position, pivot_y_position + depth, pivot_z_position + height]
+    corner_7 = [pivot_x_position + width, pivot_y_position + depth, pivot_z_position + height]
 
     vertices = np.array([corner_1, corner_2, corner_3, corner_4, corner_5, corner_6, corner_7, corner_8])
 
@@ -343,11 +357,8 @@ def plot_cuboids(vertices_list, names):
     colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
 
     for vertices, name in zip(vertices_list, names or ['']*len(vertices_list)):
-        random_index = random.randint(0, len(colors)-1)
-        color = colors[random_index]
-
+        
         for i in range(0, 4):
-            
 
             ax.plot([vertices[i][0], vertices[(i+1)%4][0]],
                     [vertices[i][1], vertices[(i+1)%4][1]],
@@ -365,8 +376,7 @@ def plot_cuboids(vertices_list, names):
             centroid = np.mean(vertices, axis=0)
             
             ax.text(centroid[0], centroid[1], centroid[2], name, fontsize=5, horizontalalignment="center")
-
-            
+ 
     ax.set_xlim([0,1000])
     ax.set_ylim([0,1000])
     ax.set_zlim([0,1000])
@@ -382,29 +392,38 @@ def __main__(equipment, pieces):
     trailer_dimensions = get_dimensions(equipment)
 
     for i in range(10):
-        packer.add_bin(Bin(f'{equipment} {i+1}', trailer_dimensions[0], trailer_dimensions[1], trailer_dimensions[2], trailer_dimensions[3]))
+      packer.add_bin(Bin(f'{equipment} {i+1}', trailer_dimensions[0], trailer_dimensions[1], trailer_dimensions[2], trailer_dimensions[3]))
 
     for piece in pieces:
-        packer.add_item(Item(piece['name'], piece['modified_width'], piece['modified_height'], piece['depth'], piece['weight']))
+      packer.add_item(Item(piece['name'], piece['modified_width'], piece['modified_height'], piece['depth'], piece['weight']))
 
     packer.pack(distribute_items=True)
 
     vertices_list = []
     names = []
-    
     j = 0
+    
     for b in packer.bins:
       if len(b.items) > 0:
         print(f"The following items will go on {b.name}")
         for item in b.items:
-            print("--->", item.name)
-            trailer = create_trailer_object(equipment, 210, j)
-            vertices_list.append(trailer)
-            names.append("")
-            vertices = create_box_object(j, item, 210)
-            vertices_list.append(vertices)
-            names.append(item.name)
+          print("--->", item.name)
+          trailer = create_trailer_object(trailer_dimensions, equipment, 210, j)
+          vertices_list.append(trailer)
+          names.append("") 
+          vertices = create_box_object(j, item, 210, 0, item.rotation_type)
+          vertices_list.append(vertices)
+          names.append(item.name)
+        unfitted = b.unfitted_items
       j=j+1
+    print("The following items did not fit on any truck:")
+    width = 0
+    for item in unfitted:
+      print("--->", item.name)
+      unfitted_vertices = create_box_object(1, item, width, 700, 0)
+      width += item.width
+      names.append(item.name)
+      vertices_list.append(unfitted_vertices)
     plot_cuboids(vertices_list, names)
         
     return
